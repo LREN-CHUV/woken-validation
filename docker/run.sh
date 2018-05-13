7f6a9beeeb4d5c26db3f41be1b7d3b8fd06a3a1b
Gabriel Ciuloaica <gabriel.ciuloaica@iquate.com>
2018-05-13 09:45:00 +0300

    the service should start java agent for kamon to work.

--- docker/run.sh
+++ docker/run.sh
@@ -6,6 +6,7 @@ JAVA="java"
 
 # -javaagent:/opt/woken/aspectjweaver.jar
 exec ${JAVA} ${JAVA_OPTIONS} \
+          -javaagent:/opt/woken/aspectjweaver.jar \
           -Djava.library.path=/lib \
           -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
           -jar woken-validation.jar
